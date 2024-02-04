2698405acde70cb9bb917860aba89d2af2f00e85
Carson Nguyen <Carson10carly@yahoo.com>
2024-02-03 21:56:32 -0600

    Updated SSVEP analysis code

--- analysis/ssvep_analysis.py
+++ analysis/ssvep_analysis.py
@@ -1,75 +1,76 @@
-import mne
-import analysis
+import glob
 import matplotlib.pyplot as plt
-import scipy.signal
-# Load data
-# Make sure you use numpy version 1.23.5
-raw_data_1 = mne.io.read_raw_gdf('StimuliVerificationTrials/11_17_23_364DEEGtests/JasonSSVEP1.gdf', eog=['sens13', 'sens14', 'sens15'], preload=True, stim_channel='Status')
-raw_data_2 = mne.io.read_raw_gdf('StimuliVerificationTrials/11_17_23_364DEEGtests/JasonSSVEP2.gdf', eog=['sens13', 'sens14', 'sens15'], preload=True, stim_channel='Status')
-raw_data_3 = mne.io.read_raw_gdf('StimuliVerificationTrials/11_17_23_364DEEGtests/JasonSSVEP3.gdf', eog=['sens13', 'sens14', 'sens15'], preload=True, stim_channel='Status')
-raw_data_4 = mne.io.read_raw_gdf('StimuliVerificationTrials/11_17_23_364DEEGtests/JasonSSVEP4.gdf', eog=['sens13', 'sens14', 'sens15'], preload=True, stim_channel='Status')
+import mne
 
-raw_data_files = [raw_data_1, raw_data_2, raw_data_3, raw_data_4]
+# Specify SSVEP data directory
+# E.g. './data/carson/ssvep/*.gdf'
+dir = './data/brian/ssvep/*.gdf'
 
-# Set montage, aka electrode positions
+# Find and list SSVEP data files
+SSVEPfiles = glob.glob(dir)
+
+# Define EEG channels of interest
+eeg_channels = [
+    'FP1', 'FPZ', 'FP2', 'F7', 'F3', 'FZ', 'F4', 'F8', 
+    'FC5', 'FC1', 'FC2', 'FC6', 'T7', 'C3', 'CZ', 'C4', 
+    'T8', 'CP5', 'CP1', 'CP2', 'CP6', 'P7', 'P3', 'PZ', 
+    'P4', 'P8', 'POZ', 'O1', 'OZ', 'O2'
+]
+eog_channels = ['sens13', 'sens14', 'sens15']
 standard_montage = mne.channels.make_standard_montage('standard_1020')
-for raw_data in raw_data_files:
-    raw_data.set_montage(standard_montage, match_case=False)
 
-# Filter data
-filtered_data = []
-for i, raw in enumerate(raw_data_files):
-    filtered_data_file = raw.copy()
-    # Add bandpass filter between 1 and 30 Hz
-    # filtered_data_file = raw.copy().filter(l_freq=1, h_freq=30, verbose='INFO')
+epochs = []
+for file in SSVEPfiles:
+    # read the raw data
+    raw = mne.io.read_raw_gdf(file, eog_channels, preload=True, stim_channel='Status')
+    # setting montage for naming
+    raw.set_montage(standard_montage, match_case=False)
+    # picking only the eeg channels of interest
+    raw.pick_channels(eeg_channels)
 
-    # Add notch filter at 60 Hz
-    filtered_data_file.notch_filter(60, verbose='INFO')
-    filtered_data.append(filtered_data_file)
+    # Apply notch filter of 60 Hz
+    raw.notch_filter(60, verbose='INFO')
 
-# Set montage, aka electrode positions
-standard_montage = mne.channels.make_standard_montage('standard_1020')
-for raw_data in raw_data_files:
-    raw_data.set_montage(standard_montage, match_case=False)
+    events, _ = mne.events_from_annotations(raw)
 
-mapping = {
-    1: '7.5 Hz',
-    2: '8.57 Hz',
-    3: '10 Hz',
-    4: '12 Hz',
-    6: 'START',
-    5: 'END'
-}
+    # Extract 7.5 Hz epochs
+    event_id = 2
+    freq = 7.5
+    epoch = mne.Epochs(raw, events, event_id, event_repeated='merge', tmin=1, tmax=8, baseline=None, verbose='INFO')
+    # Plot power spectral densitites (PSDs)
+    figure = epoch[f'{event_id}'][0].compute_psd(method='welch', fmin=0, fmax=25, tmin=0, tmax=120).plot()
+    figure.axes[0].set_title(f'SSVEP ({freq} Hz)')
+    figure.axes[0].axvline(x=freq, color='red', linestyle='--', label='1f')
+    figure.axes[0].axvline(x=freq*2, color='blue', linestyle='--', label='2f')
 
-list_of_epochs = []
+    # Extract 8.57 Hz epochs
+    event_id = 3
+    freq = 8.57
+    epoch = mne.Epochs(raw, events, event_id, event_repeated='merge', tmin=1, tmax=8, baseline=None, verbose='INFO')
+    # Plot power spectral densitites (PSDs)
+    figure = epoch[f'{event_id}'][0].compute_psd(method='welch', fmin=0, fmax=25, tmin=0, tmax=120).plot()
+    figure.axes[0].set_title(f'SSVEP ({freq} Hz)')
+    figure.axes[0].axvline(x=freq, color='red', linestyle='--', label='1f')
+    figure.axes[0].axvline(x=freq*2, color='blue', linestyle='--', label='2f')
 
-for data in filtered_data:
-    events, event_id = analysis.set_mapping(data, mapping) # type: ignore
-    epochs = mne.Epochs(data, events, event_id, event_repeated='merge', tmin=1, tmax=8, baseline=None, verbose='INFO')
-    list_of_epochs.append(epochs)
+    # Extract 10 Hz epochs
+    event_id = 4
+    freq = 10
+    epoch = mne.Epochs(raw, events, event_id, event_repeated='merge', tmin=1, tmax=8, baseline=None, verbose='INFO')
+    # Plot power spectral densitites (PSDs)
+    figure = epoch[f'{event_id}'][0].compute_psd(method='welch', fmin=0, fmax=25, tmin=0, tmax=120).plot()
+    figure.axes[0].set_title(f'SSVEP ({freq} Hz)')
+    figure.axes[0].axvline(x=freq, color='red', linestyle='--', label='1f')
+    figure.axes[0].axvline(x=freq*2, color='blue', linestyle='--', label='2f')
 
-# Plot PSDs
-freqs = [7.5, 8.57, 10, 12]
-freqs = [8.57]
+    # Extract 12 Hz epochs
+    event_id = 5
+    freq = 12
+    epoch = mne.Epochs(raw, events, event_id, event_repeated='merge', tmin=1, tmax=8, baseline=None, verbose='INFO')
+    # Plot power spectral densitites (PSDs)
+    figure = epoch[f'{event_id}'][0].compute_psd(method='welch', fmin=0, fmax=25, tmin=0, tmax=120).plot()
+    figure.axes[0].set_title(f'SSVEP ({freq} Hz)')
+    figure.axes[0].axvline(x=freq, color='red', linestyle='--', label='1f')
+    figure.axes[0].axvline(x=freq*2, color='blue', linestyle='--', label='2f')
 
-
-# https://mne.tools/stable/auto_tutorials/time-freq/50_ssvep.html#sphx-glr-auto-tutorials-time-freq-50-ssvep-py
-
-for i, epochs in enumerate(list_of_epochs):
-    for freq in freqs:
-
-        epochs[f"{freq} Hz"][0].compute_psd(method='welch').plot(dB=True)
-
-
-        # Welch's method is better for unsegemented data
-        # It involved segmented the data into smaller chunks and averaging the PSDs
-        # window = scipy.signal.get_window('hamming', 3585)
-        # epochs[f"{freq} Hz"][0].compute_psd(method='welch', n_fft= 3585, window=window).plot(dB=True)
-
-        # Multitaper is better for segmented data (OK apparently not but it's an alternative to Welch's method)
-        # It involves using different windows on the same segment of data and averaging the PSDs
-        # epochs[f"{freq} Hz"][0].compute_psd(method='multitaper', adaptive=True, low_bias=True).plot(dB=False)
-
-        plt.title(f"SSVEP{i} {freq}")
-
-plt.show()
+    plt.show()
